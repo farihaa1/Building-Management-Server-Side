c2223843b374fb26b3d23c8e9d4c05e768fa2f9d
farihaa1 <farihaa1403@gmail.com>
2025-01-18 23:26:17 +0600

    added aparment pagination routes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express= require("express") ;
+const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
-const port = process.env.PORT || 5001;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cd15p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,19 +16,40 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
-    const buildingCollection = client.db("managementDb").collection("building");
+    const apartmentCollection = client
+      .db("managementDb")
+      .collection("apartments");
+    // const userCollection = client.db("managementDb").collection("users");
 
-   
+    app.get("/apartments", async (req, res) => {
+      const { page, size } = req.query;
+      const limit = parseInt(size);
+      const skip = parseInt(page) * limit;
+
+      const apartments = await apartmentCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      res.json(apartments);
+    });
+
+    app.get("/apartmentsCount", async (req, res) => {
+      const count = await apartmentCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,13 +57,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-app.get("/", (req, res)=>{
-    res.send(`app is running `)
-})
-app.listen(port, ()=>{
-    console.log(`building management system is running in ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send(`app is running `);
+});
+app.listen(port, () => {
+  console.log(`building management system is running in ${port}`);
+});
